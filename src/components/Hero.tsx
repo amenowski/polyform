4be46f1e528db02c3e4e3c739b62e85c1fdb042d
amenowski <contactserus@gmail.com>
2024-06-17 18:02:24 +0200

    feat: create hero component and cn function

---
+++ src/components/Hero.tsx
@@ -0,0 +1,20 @@
+import HeroBg from "../assets/images/hp1.webp";
+import Button from "./ui/Button";
+
+export default function Hero() {
+  return (
+    <div className="absolute top-0 -z-50 h-[900px] w-full">
+      <img src={HeroBg} className="h-[900px] w-full object-cover" />
+      <div className="absolute inset-0 mx-auto flex max-w-[1440px] flex-col items-start justify-center gap-4 px-4 text-white">
+        <span className="text-sm uppercase">new arrivals</span>
+        <h1 className="text-left text-6xl leading-none md:text-7xl md:leading-tight">
+          Elevate your <br /> space withluxe <br /> furniture!
+        </h1>
+        <span>Explore our collections for a wider selection.</span>
+        <Button isArrow={true} to="shop">
+          Find out more
+        </Button>
+      </div>
+    </div>
+  );
+}
