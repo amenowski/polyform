7f141a4b981ccc03c443b86a2a0849db1bc67624
amenowski <contactserus@gmail.com>
2024-06-18 17:20:32 +0200

    feat: create mobile navigation and context to open and close it

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -4,62 +4,72 @@ import Container from "./ui/Container";
 import Logo from "./ui/Logo";
 
 import { CiUser, CiSearch, CiBag1, CiMenuBurger } from "react-icons/ci";
+import MobileNavigation from "./MobileNavigation";
+import { useNavigationContext } from "../hooks/NavigationContext";
 
 export default function Header() {
+  const { isNavigationOpen, setIsNavigationOpen } = useNavigationContext();
+
   const currentPage = useLocation().pathname;
   const isHomePage = currentPage === "/home";
 
-  function handleOpenMenu() {}
+  function handleOpenNavigation() {
+    setIsNavigationOpen(true);
+    console.log(isNavigationOpen);
+  }
 
   function handleOpenCart() {}
 
   function handleOpenSearch() {}
 
   return (
-    <header className="w-full border-b py-4 text-white">
-      <Container className="flex items-center justify-between">
-        <div className="flex items-center gap-12">
-          <Logo />
-          <Navigation />
-        </div>
-        <div className="flex items-center gap-2">
-          <Link to="account">
-            <CiUser
+    <>
+      <header className="relative w-full border-b py-4 text-white">
+        <Container className="flex items-center justify-between">
+          <div className="flex items-center gap-12">
+            <Logo />
+            <Navigation />
+          </div>
+          <div className="flex items-center gap-2">
+            <Link to="account">
+              <CiUser
+                className={`cursor-pointer p-2 ${
+                  isHomePage
+                    ? "hover:bg-white hover:text-black"
+                    : "text-black hover:bg-black hover:text-white"
+                }`}
+                size={45}
+              />
+            </Link>
+            <CiSearch
+              size={45}
               className={`cursor-pointer p-2 ${
+                isHomePage
+                  ? "text-white hover:bg-white hover:text-black"
+                  : "text-black hover:bg-black hover:text-white"
+              }`}
+            />
+            <div
+              className={`flex cursor-pointer items-center gap-1 p-2 ${
                 isHomePage
                   ? "hover:bg-white hover:text-black"
                   : "text-black hover:bg-black hover:text-white"
               }`}
-              size={45}
+            >
+              <CiBag1 size={30} />
+              <span>$232.20</span>
+            </div>
+            <CiMenuBurger
+              onClick={handleOpenNavigation}
+              className={`block cursor-pointer md:hidden ${
+                isHomePage ? "text-white" : "text-black"
+              }`}
+              size={30}
             />
-          </Link>
-          <CiSearch
-            size={45}
-            className={`cursor-pointer p-2 ${
-              isHomePage
-                ? "text-white hover:bg-white hover:text-black"
-                : "text-black hover:bg-black hover:text-white"
-            }`}
-          />
-          <div
-            className={`flex cursor-pointer items-center gap-1 p-2 ${
-              isHomePage
-                ? "hover:bg-white hover:text-black"
-                : "text-black hover:bg-black hover:text-white"
-            }`}
-          >
-            <CiBag1 size={30} />
-            <span>$232.20</span>
           </div>
-          <CiMenuBurger
-            onClick={handleOpenMenu}
-            className={`block cursor-pointer md:hidden ${
-              isHomePage ? "text-white" : "text-black"
-            }`}
-            size={30}
-          />
-        </div>
-      </Container>
-    </header>
+        </Container>
+      </header>
+      <MobileNavigation />
+    </>
   );
 }
---
+++ src/components/MobileNavigation.tsx
@@ -0,0 +1,57 @@
+import { useEffect, useRef } from "react";
+import { createPortal } from "react-dom";
+import { IoMdClose } from "react-icons/io";
+import { Link } from "react-router-dom";
+import { motion } from "framer-motion";
+
+import { useNavigationContext } from "../hooks/NavigationContext";
+
+import Logo from "./ui/Logo";
+
+export default function MobileNavigation() {
+  const { isNavigationOpen, setIsNavigationOpen } = useNavigationContext();
+  const initialRender = useRef(true);
+
+  useEffect(() => {
+    if (initialRender.current) {
+      initialRender.current = false;
+    }
+  }, []);
+
+  function handleToggleNavigation() {
+    setIsNavigationOpen((isOpen) => !isOpen);
+  }
+
+  const variants = {
+    open: { opacity: 1, x: 0 },
+    closed: { opacity: 0, x: "100%" },
+  };
+
+  return createPortal(
+    <motion.div
+      initial={initialRender.current ? false : "closed"}
+      variants={variants}
+      animate={isNavigationOpen ? "open" : "closed"}
+      transition={{ ease: "easeIn", duration: "0.3" }}
+      className="absolute right-0 top-0 h-screen w-[400px] bg-white p-4"
+    >
+      <div className="flex items-center justify-between border-b pb-4">
+        <Logo variant="black" />
+        <IoMdClose
+          onClick={handleToggleNavigation}
+          className="cursor-pointer"
+          size={30}
+        />
+      </div>
+      <ul className="flex flex-col gap-2 py-4 text-lg">
+        <li className="hover:bg-hoverBackground w-full cursor-pointer p-2 transition-all duration-200 hover:text-white">
+          <Link to="shop">Shop</Link>
+        </li>
+        <li className="hover:bg-hoverBackground w-full cursor-pointer p-2 transition-all duration-200 hover:text-white">
+          <Link to="login">Login</Link>
+        </li>
+      </ul>
+    </motion.div>,
+    document.body,
+  );
+}
