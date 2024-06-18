e2134707383248482aff6001de0c2e39fb4bd587
amenowski <contactserus@gmail.com>
2024-06-18 17:21:40 +0200

    style: add animation on hover and set overflow x to hidden

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,13 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.tsx'
-import './index.css'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App.tsx";
+import "./index.css";
+import NavigationContextProvider from "./hooks/NavigationContext.tsx";
 
-ReactDOM.createRoot(document.getElementById('root')!).render(
+ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
-    <App />
+    <NavigationContextProvider>
+      <App />
+    </NavigationContextProvider>
   </React.StrictMode>,
-)
+);
