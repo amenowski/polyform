4252d53ab82665b70538d0e0b04f61396c48f95b
amenowski <contactserus@gmail.com>
2024-06-29 19:21:28 +0200

    fix: change isPending to isLoading state

--- src/hooks/useBestProducts.tsx
+++ src/hooks/useBestProducts.tsx
@@ -2,10 +2,10 @@ import { useQuery } from "@tanstack/react-query";
 import { getBestProducts } from "../services/apiProducts";
 
 export function useBestProducts() {
-  const { data: bestProducts, isPending } = useQuery({
+  const { data: bestProducts, isLoading } = useQuery({
     queryKey: ["bestProducts"],
     queryFn: getBestProducts,
   });
 
-  return { bestProducts, isPending };
+  return { bestProducts, isLoading };
 }
