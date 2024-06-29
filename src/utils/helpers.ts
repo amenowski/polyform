33a2bfc47326ad4210907b009b114a2667a6aa0e
amenowski <contactserus@gmail.com>
2024-06-29 18:52:48 +0200

    feat: create 2 helpers functions to convert currencies and format url

---
+++ src/utils/helpers.ts
@@ -0,0 +1,7 @@
+export const formatCurrency = (value: number) =>
+  new Intl.NumberFormat("pl", { style: "currency", currency: "EUR" }).format(
+    value,
+  );
+export const formatNameForURL = (name: string) => {
+  return name.replace(/\s+/g, "-");
+};
