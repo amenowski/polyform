f43358ea1c529619c1ccef34213ad26aaef44c6e
amenowski <contactserus@gmail.com>
2024-06-29 17:30:48 +0200

    fix: add key

--- src/services/supabase.ts
+++ src/services/supabase.ts
@@ -1,11 +1,8 @@
 import { createClient } from "@supabase/supabase-js";
 
 const supabaseUrl = "https://dqzbjenzkjjfzhnvychp.supabase.co";
-const supabaseKey = process.env.SUPABASE_KEY as string;
-
-if (!process.env.SUPABASE_KEY) {
-  throw new Error("SUPABASE_KEY environment variable is not set");
-}
+const supabaseKey =
+  "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImRxemJqZW56a2pqZnpobnZ5Y2hwIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MTk0MTYwODQsImV4cCI6MjAzNDk5MjA4NH0.4nVVqma6dJ5oCjmJW6Jr2nSWSwGHURywdc6whTiPbYo";
 
 const supabase = createClient(supabaseUrl, supabaseKey);
 
