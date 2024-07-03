e78ed0467580047dc03cd4093a95136fd6729765
amenowski <contactserus@gmail.com>
2024-07-03 11:23:43 +0200

    feat: create function to scroll top

--- src/utils/helpers.ts
+++ src/utils/helpers.ts
@@ -8,3 +8,6 @@ export const formatNameForURL = (name: string) => {
 export const revertFormattedName = (formattedName: string) => {
   return formattedName.replace(/-/g, " ");
 };
+export const scrollTop = () => {
+  window.scrollTo({ top: 0, behavior: "smooth" });
+};
