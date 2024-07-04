429a546016c3dec1a03d9b4f9c8d8ad1508231c0
amenowski <contactserus@gmail.com>
2024-07-04 08:55:00 +0200

    feat: create functions to login and signup account with supabase

---
+++ src/services/apiAuth.ts
@@ -0,0 +1,50 @@
+import supabase from "./supabase";
+
+export async function signup(
+  name: string,
+  email: string,
+  password: string,
+  lastName: string,
+) {
+  const { data, error } = await supabase.auth.signUp({
+    email,
+    password,
+    options: {
+      data: {
+        name,
+        lastName,
+      },
+    },
+  });
+
+  if (error) throw new Error(error.message);
+
+  return data;
+}
+
+export async function login(email: string, password: string) {
+  let { data, error } = await supabase.auth.signInWithPassword({
+    email,
+    password,
+  });
+
+  if (error) throw new Error(error.message);
+
+  return data;
+}
+
+export async function getCurrentUser() {
+  const { data: session } = await supabase.auth.getSession();
+  if (!session.session) return null;
+
+  const { data, error } = await supabase.auth.getUser();
+
+  if (error) throw new Error(error.message);
+
+  return data?.user;
+}
+
+export async function logout() {
+  const { error } = await supabase.auth.signOut();
+  if (error) throw new Error(error.message);
+}
