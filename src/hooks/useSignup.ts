e5fe2e41358deca00d5036747474c4d40caec3b5
amenowski <contactserus@gmail.com>
2024-07-05 11:28:01 +0200

    fix: name of type signup props

--- src/hooks/useSignup.ts
+++ src/hooks/useSignup.ts
@@ -2,13 +2,13 @@ import { useNavigate } from "react-router-dom";
 
 import { useMutation } from "@tanstack/react-query";
 
-import { useSignupProps } from "../lib/types";
+import { SignupProps } from "../lib/types";
 import { signup as signupApi } from "../services/apiAuth";
 
 export function useSignup() {
   const navigate = useNavigate();
   const { mutate: signup, isPending } = useMutation({
-    mutationFn: ({ name, email, password, lastName }: useSignupProps) =>
+    mutationFn: ({ name, email, password, lastName }: SignupProps) =>
       signupApi(name, email, password, lastName),
     onError: (err) => {
       console.log("ERROR", err);
