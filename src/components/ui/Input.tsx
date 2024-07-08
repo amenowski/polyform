4c5edee4e6b55e7acb73b14bd6a1a6781ac5ec2e
amenowski <contactserus@gmail.com>
2024-07-08 22:29:03 +0200

    chore: add className type

--- src/components/ui/Input.tsx
+++ src/components/ui/Input.tsx
@@ -7,10 +7,20 @@ type InputProps = {
   disabled?: boolean;
   id?: string;
   onChange?: (e: React.ChangeEvent<HTMLInputElement>) => void;
+  className?: string;
 };
 
 function Input(
-  { type, value, placeholder, disabled, onChange, id, ...rest }: InputProps,
+  {
+    type,
+    value,
+    placeholder,
+    disabled,
+    onChange,
+    id,
+    className,
+    ...rest
+  }: InputProps,
   ref: React.Ref<HTMLInputElement>,
 ) {
   return (
@@ -23,7 +33,7 @@ function Input(
       onChange={onChange}
       ref={ref}
       {...rest}
-      className="w-full rounded-sm border px-4 py-3 outline-none transition-all duration-300 placeholder:text-black hover:border-black focus:border-gray-400 focus:ring-4 focus:ring-gray-400"
+      className={`w-full rounded-sm border px-4 py-3 outline-none transition-all duration-300 placeholder:text-black hover:border-black focus:border-gray-400 focus:ring-4 focus:ring-gray-400 ${className}`}
     />
   );
 }
