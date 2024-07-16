089c6cc5aa69b42d158440ad7a0a3abb680a2b18
amenowski <contactserus@gmail.com>
2024-07-16 17:04:39 +0200

    style: add fixed height of image

--- src/components/products/ProductItem.tsx
+++ src/components/products/ProductItem.tsx
@@ -9,7 +9,7 @@ export default function ProductItem({ product }: { product: ProductT }) {
   return (
     <div className="group/product relative py-4">
       <Link to={`/product/${formatNameForURL(name)}`}>
-        <img src={images[0]} alt={name} />
+        <img className="h-[17rem]" src={images[0]} alt={name} />
         <img
           src={images[1]}
           className="absolute left-0 top-0 hidden group-hover/product:block"
