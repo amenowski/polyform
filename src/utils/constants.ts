fded0ac3681cbeb994eb95085b62d0886d104633
amenowski <contactserus@gmail.com>
2024-08-02 09:20:12 +0200

    refactor: extract categories to consttants folder

--- src/utils/constants.ts
+++ src/utils/constants.ts
@@ -1,1 +1,29 @@
 export const PAGE_SIZE = 4;
+export type CATEGORY =
+  | "sofas"
+  | "beds"
+  | "armchairs"
+  | "poufs"
+  | "livingroom"
+  | "bedroom"
+  | "diningroom"
+  | "office"
+  | "modern"
+  | "traditional"
+  | "vintage"
+  | "scandinavian";
+
+export const CATEGORIESARR: CATEGORY[] = [
+  "sofas",
+  "beds",
+  "armchairs",
+  "poufs",
+  "livingroom",
+  "bedroom",
+  "diningroom",
+  "office",
+  "modern",
+  "traditional",
+  "vintage",
+  "scandinavian",
+];
